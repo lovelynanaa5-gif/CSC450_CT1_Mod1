2528da0f6d294946c4aaf5383766acd17bae2dab
lovelynanaa5-gif <lovelynanaa5@gmail.com>
2025-09-14 23:17:32 -0600

    Create PersonInfo.cpp

---
+++ PersonInfo.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string firstName   = "Alex";
+    string lastName    = "Carter";
+    string street      = "1234 Elm Street";
+    string city        = "Aurora";
+    string zipCode     = "80014";
+
+    cout << "First Name: "   << firstName << endl;
+    cout << "Last  Name: "   << lastName  << endl;
+    cout << "Street Address: " << street  << endl;
+    cout << "City: "         << city      << endl;
+    cout << "Zip code: "     << zipCode   << endl;
+
+    return 0;
+}
