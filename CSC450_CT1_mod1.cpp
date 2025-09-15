90439a9f4b79905329718c579ab53a45e064d9a3
lovelynanaa5-gif <lovelynanaa5@gmail.com>
2025-09-14 23:18:03 -0600

    Create CSC450_CT1_mod1.cpp

---
+++ CSC450_CT1_mod1.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Welcome to this C++ Program" << endl;
+    cout << "I have corrected all errors for this program." << endl;
+    return 0;
+}
