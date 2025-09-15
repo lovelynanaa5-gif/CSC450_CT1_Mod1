c83ef28640e82b36a6dee2af4af21007fbacfd7b
lovelynanaa5-gif <lovelynanaa5@gmail.com>
2025-09-14 23:18:23 -0600

    Create CSC450_CT1_mod1-2.cpp

---
+++ CSC450_CT1_mod1-2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double myMoney = 1000.50;  // this should be printed out
+
+    cout << "Please be sure to correct all syntax errors in this program" << endl;
+    cout << "I have corrected all errors for this program." << endl;
+    cout << "The total amount of money available is = " << myMoney << endl;
+
+    return 0;
+}
